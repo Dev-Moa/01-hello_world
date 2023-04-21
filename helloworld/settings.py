be66c796077541d439137bc51a02ac5ff63ccd1b
Dev Moha <115456273+Dev-Moa@users.noreply.github.com>
2023-04-21 21:11:43 +0300

    Add files via upload

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gijrna_se#dc&zx&7e03yi6-i_(k^fa0)q+)fovr%t=@yt&%rq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['01-helloworld-production.up.railway.app']
 
 
 # Application definition
